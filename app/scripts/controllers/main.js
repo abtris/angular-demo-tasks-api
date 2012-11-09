a727f061ee4352c0b6ac8d72dc4fe2987cfd9a0e
Ladislav Prskavec <ladislav@prskavec.net>
2012-11-09 19:07:26 +0100

    Init test with apiary.io

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,4 +6,14 @@ angularDemoTasksApiApp.controller('MainCtrl', function($scope) {
     'AngularJS',
     'Testacular'
   ];
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://private-b5e4-abtris.apiary.io/tasks/');
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      alert('Status: '+this.status+'\nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'\nBody: '+this.responseText);
+    }
+  };
+  xhr.send("");
 });
