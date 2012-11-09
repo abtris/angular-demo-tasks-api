5690423d58cb100d7008d9f230c0fabd31026961
Ladislav Prskavec <ladislav@prskavec.net>
2012-11-09 18:58:50 +0100

    yeoman init angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angularDemoTasksApiApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
