9ec58513350f6b7d1b6bd28d1245fa53eff585fe
Ladislav Prskavec <ladislav@prskavec.net>
2012-11-09 20:15:08 +0100

    - Add GetAll and GetTask - removed GA

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,23 @@
 
 angularDemoTasksApiApp.controller('MainCtrl', function($scope, $http) {
 
-  $scope.status = "";
-  $http.get('http://private-b5e4-abtris.apiary.io/tasks/').success(function(data) {
-    $scope.status = data;
-  });
+  $scope.getAll = function () {
+    $http.get('http://private-b5e4-abtris.apiary.io/tasks/').
+      success(function(data) {
+        $scope.tasks = data;
+        $scope.statusHidden = false;
+        $scope.taskHidden = true;
+    });
+  };
 
+  $scope.getTask = function (id) {
+    $http.get('http://private-b5e4-abtris.apiary.io/task/' + id).
+      success(function(data) {
+        $scope.status = data;
+        $scope.statusHidden = true;
+        $scope.taskHidden = false;
+    });
+  };
 
 });
+
