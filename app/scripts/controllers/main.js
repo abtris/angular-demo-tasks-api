f7e792bb6b0818d1a87ad244aefb4bc245b960be
Ladislav Prskavec <ladislav@prskavec.net>
2012-11-10 16:51:26 +0100

    Add more methods

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angularDemoTasksApiApp.controller('MainCtrl', function($scope, $http) {
+  $scope.url = "http://private-b5e4-abtris.apiary.io";
 
   $scope.getAll = function () {
-    $http.get('http://private-b5e4-abtris.apiary.io/tasks/').
+    $http.get($scope.url + '/tasks/').
       success(function(data) {
         $scope.tasks = data;
         $scope.statusHidden = false;
@@ -12,12 +13,24 @@ angularDemoTasksApiApp.controller('MainCtrl', function($scope, $http) {
   };
 
   $scope.getTask = function (id) {
-    $http.get('http://private-b5e4-abtris.apiary.io/task/' + id).
+    $http.get($scope.url + '/task/' + id).
       success(function(data) {
         $scope.status = data;
         $scope.statusHidden = true;
         $scope.taskHidden = false;
     });
+  };
+
+  $scope.updateTask = function (data, id) {
+    $http.put($scope.url + '/task/' + id);
+  };
+
+  $scope.createTask = function (data) {
+    $http.post($scope.url + '/task/', data).success(function () {});
+  };
+
+  $scope.deleteTask = function (id) {
+    $http.delete($scope.url + '/task/' + id);
   };
 
 });
